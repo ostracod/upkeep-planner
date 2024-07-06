63137e214c2697c59c858b4bc76c98384091ac5a
John Eisenmann <esperantanaso@gmail.com>
2024-07-06 11:46:43 -0400

    Allow moving planner items

--- public/javascript/tasks.js
+++ public/javascript/tasks.js
@@ -20,20 +20,31 @@ class Container {
     constructor(tag) {
         this.tag = tag;
         this.plannerItems = [];
+        this.parentPlannerItem = null;
     }
     
-    addItem(plannerItem) {
+    addItem(plannerItem, index = null) {
         if (plannerItem.parentContainer !== null) {
             plannerItem.remove();
         }
-        this.tag.appendChild(plannerItem.tag);
-        this.plannerItems.push(plannerItem);
+        if (index === null || index >= this.plannerItems.length) {
+            this.tag.appendChild(plannerItem.tag);
+            this.plannerItems.push(plannerItem);
+        } else {
+            const nextItem = this.plannerItems[index];
+            this.tag.insertBefore(plannerItem.tag, nextItem.tag);
+            this.plannerItems.splice(index, 0, plannerItem);
+        }
         plannerItem.parentContainer = this;
+    }
+    
+    findItem(plannerItem) {
+        return this.plannerItems.indexOf(plannerItem);
     }
     
     removeItem(plannerItem) {
         this.tag.removeChild(plannerItem.tag);
-        const index = this.plannerItems.indexOf(plannerItem);
+        const index = this.findItem(plannerItem);
         this.plannerItems.splice(index, 1);
         plannerItem.parentContainer = null;
     }
@@ -54,14 +65,112 @@ class PlannerItem {
             ...buttonDefs,
             {
                 text: "Move",
-                onClick: () => {},
+                onClick: () => {
+                    this.startMove();
+                },
             },
         ]);
         return this.buttonsTag;
     }
     
+    createMoveButtons() {
+        this.moveButtonsTag = createButtons([
+            {
+                text: "Up",
+                onClick: () => {
+                    this.moveUp();
+                },
+            },
+            {
+                text: "Down",
+                onClick: () => {
+                    this.moveDown();
+                },
+            },
+            {
+                text: "Enter",
+                onClick: () => {
+                    this.enterCategory();
+                },
+            },
+            {
+                text: "Exit",
+                onClick: () => {
+                    this.exitCategory();
+                },
+            },
+            {
+                text: "End Move",
+                onClick: () => {
+                    this.endMove();
+                },
+            },
+        ]);
+        this.moveButtonsTag.style.display = "none";
+        return this.moveButtonsTag;
+    }
+    
     remove() {
         this.parentContainer.removeItem(this);
+    }
+    
+    startMove() {
+        this.buttonsTag.style.display = "none";
+        this.moveButtonsTag.style.display = "";
+    }
+    
+    endMove() {
+        this.buttonsTag.style.display = "";
+        this.moveButtonsTag.style.display = "none";
+    }
+    
+    getIndex() {
+        return this.parentContainer.findItem(this);
+    }
+    
+    moveUp() {
+        const nextIndex = this.getIndex() - 1;
+        if (nextIndex < 0) {
+            return;
+        }
+        const container = this.parentContainer;
+        this.remove();
+        container.addItem(this, nextIndex);
+    }
+    
+    moveDown() {
+        const nextIndex = this.getIndex() + 1;
+        const container = this.parentContainer;
+        if (nextIndex >= container.plannerItems.length) {
+            return;
+        }
+        this.remove();
+        container.addItem(this, nextIndex);
+    }
+    
+    enterCategory() {
+        const nextIndex = this.getIndex() + 1;
+        const { plannerItems } = this.parentContainer;
+        if (nextIndex >= plannerItems.length) {
+            return;
+        }
+        const nextItem = plannerItems[nextIndex];
+        if (!(nextItem instanceof Category)) {
+            return;
+        }
+        this.remove();
+        nextItem.addItem(this, 0);
+    }
+    
+    exitCategory() {
+        const { parentPlannerItem } = this.parentContainer;
+        if (parentPlannerItem === null) {
+            return;
+        }
+        const container = parentPlannerItem.parentContainer;
+        const index = container.findItem(parentPlannerItem);
+        this.remove();
+        container.addItem(this, index);
     }
 }
 
@@ -83,6 +192,7 @@ class Category extends PlannerItem {
         this.nameTag = document.createElement("span");
         this.nameTag.innerHTML = this.name;
         this.nameTag.style.marginRight = "15px";
+        this.nameTag.style.fontWeight = "bold";
         rowTag.appendChild(this.nameTag);
         this.renameTag = document.createElement("input");
         this.renameTag.style.width = "150px";
@@ -119,6 +229,8 @@ class Category extends PlannerItem {
             },
         ]);
         rowTag.appendChild(buttonsTag);
+        const moveButtonsTag = this.createMoveButtons();
+        rowTag.appendChild(moveButtonsTag);
         this.renameButtonsTag = createButtons([
             {
                 text: "Save",
@@ -140,14 +252,19 @@ class Category extends PlannerItem {
         const containerTag = document.createElement("div");
         containerTag.style.marginLeft = "20px";
         this.container = new Container(containerTag);
+        this.container.parentPlannerItem = this;
         output.appendChild(containerTag);
         
         return output;
     }
     
+    addItem(plannerItem, index = null) {
+        this.container.addItem(plannerItem, index);
+    }
+    
     addNewCategory() {
         const category = new Category(newCategoryName);
-        this.container.addItem(category);
+        this.addItem(category);
     }
     
     startRename() {
